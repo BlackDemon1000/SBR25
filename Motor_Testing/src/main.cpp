a59ec342957b70020775e0247bed3d31dd37c510
BlackDemon1000 <cb1.10.10.10@outlook.de>
2025-03-31 12:30:12 +0200

    lib deps and revoke

--- Motor_Testing/src/main.cpp
+++ Motor_Testing/src/main.cpp
@@ -1,4 +1,20 @@
 #include <DynamixelShield.h>
+#include <Arduino.h>
+#include <VL53L0X.h>
+#include <Wire.h>
+
+VL53L0X sensor1;
+VL53L0X sensor2;
+
+enum Motor {
+  Motor_Links,
+  Motor_Rechts,
+  check,
+};
+
+#define XSHUT_FRONT 1
+#define XSHUT_BACK 2
+//#define LED_BUILTIN 13
 
 #if defined(ARDUINO_AVR_UNO) || defined(ARDUINO_AVR_MEGA2560)
   #include <SoftwareSerial.h>
@@ -10,8 +26,10 @@
   #define DEBUG_SERIAL Serial
 #endif
 
-const uint8_t DXL_ID = 1;
-const float DXL_PROTOCOL_VERSION = 2.0;
+const uint8_t Motor_links = 2;
+const uint8_t Motor_rechts = 1;
+
+const float DXL_PROTOCOL_VERSION = 1.0; //Wichtig, nicht 2.0
 
 DynamixelShield dxl;
 
@@ -19,80 +37,108 @@ DynamixelShield dxl;
 using namespace ControlTableItem;
 
 void setup() {
-  // put your setup code here, to run once:
+
   
   // For Uno, Nano, Mini, and Mega, use UART port of DYNAMIXEL Shield to debug.
-  DEBUG_SERIAL.begin(115200);
-  
+  DEBUG_SERIAL.begin(19200); //Die Baudrate nehmen sonst stirb er iwi
+  /*Wire.begin();
+
+    // tof
+    if (!sensor1.init()) {
+      Serial.println("tof nciht gefunden");
+      while (1);
+  }*/
+  /*sensor1.setTimeout(500);
+  sensor1.startContinuous();*/
+  DEBUG_SERIAL.println("tof initialisiert");
+  pinMode(LED_BUILTIN, OUTPUT);
+
+
   // Set Port baudrate to 57600bps. This has to match with DYNAMIXEL baudrate.
-  dxl.begin(57600);
+  dxl.begin(1000000);
   // Set Port Protocol Version. This has to match with DYNAMIXEL protocol version.
   dxl.setPortProtocolVersion(DXL_PROTOCOL_VERSION);
   // Get DYNAMIXEL information
-  dxl.ping(DXL_ID);
+  dxl.ping(Motor_links);
+  dxl.ping(Motor_rechts);
+
+  // Turn off torque when configuring items in EEPROM area
+  dxl.torqueOff(Motor_links);
+  dxl.torqueOff(Motor_rechts);
+  dxl.setOperatingMode(Motor_links, OP_VELOCITY);
+  dxl.setOperatingMode(Motor_rechts, OP_VELOCITY);
+  dxl.torqueOn(Motor_links);
+  dxl.torqueOn(Motor_rechts);
+  //Sensors Start:
+  /*pinMode(XSHUT_FRONT, OUTPUT);
+  pinMode(XSHUT_BACK, OUTPUT);
+  digitalWrite(XSHUT_BACK, LOW);
+  digitalWrite(XSHUT_BACK, HIGH);
+  delay(10);
+  sensor2.setAddress(0x30);
+  sensor2.startContinuous();*/
+}
+float stop = -100;
+float msl = -15.0;  //max speed links
+float msr = 85.0;   //max speed rechts
+
+int Motorlinks_Geschw = 100;   //Variable für lesen aktuelle RPM Motor links
+int Motorrechts_Geschw = 100;  //Variable für lesen aktuelle RPM Motor rechts
+
+// returns distance in mm, or -1 if timeout
+unsigned long getDistance() {
+  unsigned long distance = sensor1.readRangeContinuousMillimeters();
+  if (sensor1.timeoutOccurred()) {
+    return -1;
+  } else {
+    return distance;
+  }
+}
+
+void motorController(double Speed, Motor motorlocation) {
+  double correctedSpeed = 0;
+  switch (motorlocation)
+  {
+  case Motor_Links:
+    if (Speed > 0){
+      correctedSpeed = 100 - Speed; //greislig ich weiß
+    }
+    else {
+      correctedSpeed = (100 - Speed) * -1; //greislig ich weiß
+    }
+    dxl.setGoalVelocity(Motor_links, correctedSpeed, UNIT_PERCENT);
+    break;
+  case Motor_Rechts:
+    if (Speed < 0){
+      correctedSpeed = 100 - Speed; //greislig ich weiß
+    }
+    else {
+      correctedSpeed = (100 - Speed) * -1; //greislig ich weiß
+    }
+    dxl.setGoalVelocity(Motor_rechts, correctedSpeed, UNIT_PERCENT);
+    break;
+  case check:
+    //Control loop
+    Motorlinks_Geschw = dxl.getPresentVelocity(1);
+    Motorrechts_Geschw = dxl.getPresentVelocity(2);
+    dxl.setGoalVelocity(Motor_rechts, Motorrechts_Geschw, UNIT_RPM);
+    dxl.setGoalVelocity(Motor_links, Motorlinks_Geschw, UNIT_RPM);
+    break;
+  }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  
-  // Position Control Mode in protocol2.0, Joint Mode in protocol1.0
-  // Turn off torque when configuring items in EEPROM area
-  dxl.torqueOff(DXL_ID);
-  dxl.setOperatingMode(DXL_ID, OP_POSITION);
-  dxl.torqueOn(DXL_ID);
-  if(dxl.setGoalPosition(DXL_ID, 512)){
-    delay(1000);
-    DEBUG_SERIAL.print("Present Position : ");
-    DEBUG_SERIAL.println(dxl.getPresentPosition(DXL_ID)); DEBUG_SERIAL.println();
+  /*if(getDistance() <= 200) {
+    digitalWrite(LED_BUILTIN, HIGH);
   }
-
-  // Velocity Contorl Mode in protocol2.0, Speed Mode in protocol1.0
-  dxl.torqueOff(DXL_ID);
-  dxl.setOperatingMode(DXL_ID, OP_VELOCITY);
-  dxl.torqueOn(DXL_ID);
-  if(dxl.setGoalVelocity(DXL_ID, 128)){
-    delay(1000);
-    DEBUG_SERIAL.print("Present Velocity : ");
-    DEBUG_SERIAL.println(dxl.getPresentVelocity(DXL_ID)); DEBUG_SERIAL.println();
+  else {
+    digitalWrite(LED_BUILTIN, LOW);
   }
-
-  // Extended Position Contorl Mode in protocol2.0, Multi-turn Mode(only MX series) in protocol1.0
-  dxl.torqueOff(DXL_ID);
-  dxl.setOperatingMode(DXL_ID, OP_EXTENDED_POSITION);
-  dxl.torqueOn(DXL_ID);
-  if(dxl.setGoalPosition(DXL_ID, 4096)){
-    delay(1000);
-    DEBUG_SERIAL.print("Present Extended Position : ");
-    DEBUG_SERIAL.println(dxl.getPresentPosition(DXL_ID)); DEBUG_SERIAL.println();
-  }
-
-  // PWM Contorl Mode in protocol2.0
-  dxl.torqueOff(DXL_ID);
-  dxl.setOperatingMode(DXL_ID, OP_PWM);
-  dxl.torqueOn(DXL_ID);
-  if(dxl.setGoalPWM(DXL_ID, 200)){
-    delay(1000);
-    DEBUG_SERIAL.print("Present PWM : ");
-    DEBUG_SERIAL.println(dxl.getPresentPWM(DXL_ID)); DEBUG_SERIAL.println();
-  }
-  
-  // Current Contorl Mode in protocol2.0, Torque Mode(only MX64,MX106) in protocol1.0
-  dxl.torqueOff(DXL_ID);
-  dxl.setOperatingMode(DXL_ID, OP_CURRENT);
-  dxl.torqueOn(DXL_ID);
-  if(dxl.setGoalCurrent(DXL_ID, 256)){
-    delay(1000);
-    DEBUG_SERIAL.print("Present Current : ");
-    DEBUG_SERIAL.println(dxl.getPresentCurrent(DXL_ID)); DEBUG_SERIAL.println();
-  }
-  
-  // Current Based Postion Contorl Mode in protocol2.0 (except MX28, XL430)
-  dxl.torqueOff(DXL_ID);
-  dxl.setOperatingMode(DXL_ID, OP_CURRENT_BASED_POSITION);
-  dxl.torqueOn(DXL_ID);
-  if(dxl.setGoalPosition(DXL_ID, 8192)){
-    delay(1000);
-    DEBUG_SERIAL.print("Present Current Based Position : ");
-    DEBUG_SERIAL.println(dxl.getPresentPosition(DXL_ID)); DEBUG_SERIAL.println();
-  }
+  yield();
+  Serial.println(getDistance());
+  delay(50);*/
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(100);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(100);
 }
